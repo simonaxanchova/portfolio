7037a5921ea7ae74c2a10eded25642f648c1b2eb
simonaxanchova <anchovasimona@yahoo.com>
2025-02-28 14:20:52 +0100

    Layout responsiveness improvement

--- src/modules/home/Home.js
+++ src/modules/home/Home.js
@@ -172,168 +172,6 @@ const getRandomPixelShadow = () => {
     .join(", ");
 };
 
-const pixels = [
-  "47px 140px 0 0 rgba(225, 125, 31, 1)",
-  "77px 140px 0 0 rgba(253, 197, 145, 1)",
-
-  "77px 170px 0 0 rgba(253, 197, 145, 1)",
-
-  "77px 200px 0 0 rgba(251, 139, 35, 1)",
-  "107px 200px 0 0 rgba(253, 197, 145, 1)",
-
-  "77px 230px 0 0 rgba(225, 125, 31, 1)",
-  "107px 230px 0 0 rgba(253, 197, 145, 1)",
-  "137px 230px 0 0 rgba(254, 231, 211, 1)",
-
-  "77px 260px 0 0 rgba(253, 197, 145, 1)",
-  "107px 260px 0 0 rgba(251, 139, 35, 1)",
-  "137px 260px 0 0 rgba(252, 173, 101, 1)",
-  "167px 260px 0 0 rgba(253, 197, 145, 1)",
-
-  "47px 290px 0 0 rgba(251, 139, 35, 1)",
-  "77px 290px 0 0 rgba(254, 231, 211, 1)",
-  "107px 290px 0 0 rgba(253, 197, 145, 1)",
-  "137px 290px 0 0 rgba(254, 231, 211, 1)",
-  "167px 290px 0 0 rgba(253, 208, 167, 1)",
-  "197px 290px 0 0 rgb(139, 139, 139)",
-
-  "-13px 320px 0 0 rgba(252, 173, 101, 1)",
-  "17px 320px 0 0 rgba(253, 197, 145, 1)",
-  "47px 320px 0 0 rgb(139, 139, 139)",
-  "77px 320px 0 0 rgba(253, 197, 145, 1)",
-  "107px 320px 0 0 rgba(254, 231, 211, 1)",
-  "137px 320px 0 0 rgba(251, 139, 35, 1)",
-  "167px 320px 0 0 rgba(254, 231, 211, 1)",
-  "197px 320px 0 0 rgba(253, 197, 145, 1)",
-  "227px 320px 0 0 rgb(139, 139, 139)",
-
-  "-73px 350px 0 0 rgba(253, 197, 145, 1)",
-  "-43px 350px 0 0 rgba(225, 125, 31, 1)",
-  "-13px 350px 0 0 rgb(139, 139, 139)",
-  "17px 350px 0 0 rgba(253, 197, 145, 1)",
-  "47px 350px 0 0 rgba(251, 139, 35, 1)",
-  "77px 350px 0 0 rgba(254, 231, 211, 1)",
-  "107px 350px 0 0 rgba(253, 208, 167, 1)",
-  "137px 350px 0 0 rgba(254, 231, 211, 1)",
-  "167px 350px 0 0 rgba(252, 173, 101, 1)",
-  "197px 350px 0 0 rgba(225, 125, 31, 1)",
-  "227px 350px 0 0 rgba(253, 197, 145, 1)",
-  "257px 350px 0 0 rgba(254, 231, 211, 1)",
-
-  "-43px 380px 0 0 rgba(253, 197, 145, 1)",
-  "-13px 380px 0 0 rgba(225, 125, 31, 1)",
-  "17px 380px 0 0 rgba(253, 197, 145, 1)",
-  "47px 380px 0 0 rgba(254, 231, 211, 1)",
-  "77px 380px 0 0 rgba(253, 197, 145, 1)",
-  "107px 380px 0 0 rgba(251, 139, 35, 1)",
-  "137px 380px 0 0 rgba(253, 208, 167, 1)",
-  "167px 380px 0 0 rgb(139, 139, 139)",
-  "197px 380px 0 0 rgba(253, 197, 145, 1)",
-  "227px 380px 0 0 rgba(251, 139, 35, 1)",
-  "257px 380px 0 0 rgba(252, 173, 101, 1)",
-  "287px 380px 0 0 rgba(253, 197, 145, 1)",
-
-  "-13px 410px 0 0 rgba(253, 197, 145, 1)",
-  "17px 410px 0 0 rgba(251, 139, 35, 1)",
-  "47px 410px 0 0 rgb(139, 139, 139)",
-  "77px 410px 0 0 rgba(252, 173, 101, 1)",
-  "107px 410px 0 0 rgba(225, 125, 31, 1)",
-  "137px 410px 0 0 rgba(253, 197, 145, 1)",
-  "167px 410px 0 0 rgba(254, 231, 211, 1)",
-  "197px 410px 0 0 rgba(251, 139, 35, 1)" /*tuka*/,
-  "227px 410px 0 0 rgba(253, 208, 167, 1)",
-
-  "47px 440px 0 0 rgba(251, 139, 35, 1)",
-  "77px 440px 0 0 rgba(254, 231, 211, 1)",
-  "107px 440px 0 0 rgba(253, 220, 189, 1)",
-  "137px 440px 0 0 rgba(254, 231, 211, 1)",
-  "167px 440px 0 0 rgba(253, 197, 145, 1)",
-  "197px 440px 0 0 rgba(251, 139, 35, 1)",
-  "227px 440px 0 0 rgba(253, 208, 167, 1)",
-
-  "47px 470px 0 0 rgb(139, 139, 139)",
-  "77px 470px 0 0 rgba(253, 197, 145, 1)",
-  "107px 470px 0 0 rgba(254, 231, 211, 1)",
-  "137px 470px 0 0 rgba(251, 139, 35, 1)",
-  "167px 470px 0 0 rgba(254, 231, 211, 1)",
-  "197px 470px 0 0 rgba(253, 197, 145, 1)",
-  "227px 470px 0 0 rgba(251, 139, 35, 1)",
-
-  "47px 500px 0 0 rgba(254, 231, 211, 1)",
-  "77px 500px 0 0 rgba(253, 197, 145, 1)",
-  "107px 500px 0 0 rgba(251, 139, 35, 1)",
-  "137px 500px 0 0 rgba(253, 208, 167, 1)",
-  "167px 500px 0 0 rgb(139, 139, 139)",
-  "197px 500px 0 0 rgba(253, 197, 145, 1)",
-  "227px 500px 0 0 rgba(251, 139, 35, 1)",
-
-  "17px 530px 0 0 rgba(253, 197, 145, 1)",
-  "47px 530px 0 0 rgba(225, 125, 31, 1)",
-  "77px 530px 0 0 rgba(252, 173, 101, 1)",
-  "107px 530px 0 0 rgba(254, 231, 211, 1)",
-  "137px 530px 0 0 rgba(251, 139, 35, 1)",
-  "167px 530px 0 0 rgba(254, 231, 211, 1)",
-  "197px 530px 0 0 rgba(225, 125, 31, 1)",
-  "227px 530px 0 0 rgba(253, 197, 145, 1)",
-  "257px 530px 0 0 rgba(251, 139, 35, 1)",
-
-  "17px 560px 0 0 rgba(252, 173, 101, 1)",
-  "47px 560px 0 0 rgba(253, 197, 145, 1)",
-  "77px 560px 0 0 rgba(225, 125, 31, 1)",
-  "107px 560px 0 0 rgba(253, 197, 145, 1)",
-  "137px 560px 0 0 rgba(251, 139, 35, 1)",
-  "167px 560px 0 0 rgba(253, 208, 167, 1)",
-  "197px 560px 0 0 rgb(139, 139, 139)",
-  "227px 560px 0 0 rgba(253, 197, 145, 1)",
-  "257px 560px 0 0 rgba(251, 139, 35, 1)",
-  "287px 560px 0 0 rgba(253, 197, 145, 1)",
-
-  "-13px 590px 0 0 rgba(225, 125, 31, 1)",
-  "17px 590px 0 0 rgba(254, 231, 211, 1)",
-  "47px 590px 0 0 rgba(253, 197, 145, 1)",
-  "77px 590px 0 0 rgba(251, 139, 35, 1)",
-  "107px 590px 0 0 rgba(253, 208, 167, 1)",
-  "137px 590px 0 0 rgb(139, 139, 139)",
-  "167px 590px 0 0 rgba(253, 197, 145, 1)",
-  "197px 590px 0 0 rgba(251, 139, 35, 1)",
-  "227px 590px 0 0 rgba(253, 208, 167, 1)",
-  "257px 590px 0 0 rgba(225, 125, 31, 1)",
-  "287px 590px 0 0 rgba(253, 197, 145, 1)",
-  "317px 590px 0 0 rgba(251, 139, 35, 1)",
-
-  "-43px 620px 0 0 rgba(251, 139, 35, 1)",
-  "-13px 620px 0 0 rgb(139, 139, 139)",
-  "17px 620px 0 0 rgba(225, 125, 31, 1)",
-  "47px 620px 0 0 rgba(251, 139, 35, 1)",
-  "77px 620px 0 0 rgba(252, 173, 101, 1)",
-  "107px 620px 0 0 rgba(253, 208, 167, 1)",
-  "137px 620px 0 0 rgba(251, 139, 35, 1)",
-  "167px 620px 0 0 rgba(253, 197, 145, 1)",
-  "197px 620px 0 0 rgba(251, 139, 35, 1)",
-  "227px 620px 0 0 rgba(253, 197, 145, 1)",
-
-  "77px 650px 0 0 rgba(253, 197, 145, 1)",
-  "107px 650px 0 0 rgba(251, 139, 35, 1)",
-  "137px 650px 0 0 rgba(253, 208, 167, 1)",
-  "167px 650px 0 0 rgba(253, 197, 145, 1)",
-  "197px 650px 0 0 rgba(253, 197, 145, 1)",
-
-  "107px 680px 0 0 rgba(253, 197, 145, 1)",
-  "137px 680px 0 0 rgba(251, 139, 35, 1)",
-  "167px 680px 0 0 rgba(253, 197, 145, 1)",
-
-  "107px 710px 0 0 rgba(225, 125, 31, 1)",
-  "137px 710px 0 0 rgba(253, 197, 145, 1)",
-  "167px 710px 0 0 rgba(251, 139, 35, 1)",
-
-  "107px 740px 0 0 rgba(253, 197, 145, 1)",
-  "137px 740px 0 0 rgba(251, 139, 35, 1)",
-
-  "47px 770px 0 0 rgba(253, 197, 145, 1)",
-  "77px 770px 0 0 rgba(253, 197, 145, 1)",
-  "107px 770px 0 0 rgba(251, 139, 35, 1)",
-];
-
 const colors = [
   "rgba(251, 139, 35, 1)",
   "rgba(225, 125, 31, 1)",
@@ -599,127 +437,129 @@ export default function Home() {
       <Hidden smUp>
         <Grid
           container
-          spacing={0}
+          direction="column"
+          justifyContent="space-between"
           style={{
             background: `url(${process.env.PUBLIC_URL}/assets/mobile1.png) no-repeat`,
             backgroundSize: "280% auto",
-            minHeight: "90vh",
+            minHeight: "100vh",
             backgroundPosition: "center center",
             backgroundAttachment: "fixed",
           }}
         >
-          <Grid
-            item
-            xs={12}
-            style={{
-              alignSelf: "flex-start",
-              height: "auto",
-              minHeight: 0,
-            }}
-          >
-            <div style={{ display: "flex" }}>
-              <Grid
-                item
-                xs={6}
-                id="header-left-grid"
-                style={{
-                  alignSelf: "flex-start",
-                  height: "auto",
-                }}
-              >
-                <Typography id="header-left-title">
-                  Design & Development
-                </Typography>
-                <div id="header-left-div">
-                  <div id="header-green-cube" />
-                  <Typography id="header-left-subtitle">
-                    Available for freelance
-                  </Typography>
-                </div>
-              </Grid>
-              <Grid
-                item
-                xs={6}
-                style={{
-                  display: "flex",
-                  justifyContent: "start",
-                  flexDirection: "column",
-                  alignItems: "flex-end",
-                  alignSelf: "flex-start",
-                  height: "auto",
-                }}
-              >
-                <Typography id="header-right-title">@ 2025</Typography>
-                <Typography id="header-right-subtitle">
-                  All rights reserved.
-                </Typography>
-              </Grid>
-            </div>
+          <Grid item xs={12} style={{ padding: "16px", display: "flex" }}>
             <Grid
               item
-              xs={12}
-              id="header-main-content"
-              style={{ marginTop: "10px" }}
+              xs={6}
+              id="header-left-grid"
+              style={{
+                alignSelf: "flex-start",
+                height: "auto",
+              }}
             >
-              {items.map((item) => (
-                <div
-                  className="header-outside-link-div"
-                  onClick={() => {
-                    if (item.name === "Contact") {
-                      window.location.href = item.link;
-                      return;
-                    } else {
-                      window.open(item.link, "_blank");
-                    }
-                  }}
-                >
-                  <Typography id="header-outside-link">
-                    [{item.name.slice(0, 1)}] {item.name}
-                  </Typography>
-                </div>
-              ))}
-            </Grid>
-            <Grid item xs={12}>
-              <Typography
-                className="typography-invert"
-                style={{
-                  fontFamily: "NeueHaasDisplayLight",
-                  fontSize: "55px",
-                  color: "#fff",
-                  textTransform: "uppercase",
-                  lineHeight: 1,
-                  padding: "20px 15px 0 15px",
-                  textAlign: "left",
-                }}
-              >
-                Simona
-                <br />
-                <span style={{ marginLeft: "40px" }}>Anchova</span>
+              <Typography id="header-left-title">
+                Design & Development
               </Typography>
+              <div id="header-left-div">
+                <div id="header-green-cube" />
+                <Typography id="header-left-subtitle">
+                  Available for freelance
+                </Typography>
+              </div>
             </Grid>
-            <Grid item xs={12}>
-              <Typography
-                className="typography-invert"
-                style={{
-                  fontFamily: "Inter-Thin",
-                  fontSize: "16px",
-                  color: "#fff",
-                  lineHeight: 1.1,
-                  padding: 0,
-                  textAlign: "justify",
-                  padding: "10px 15px 0 15px",
-                }}
-              >
-                Skilled Software Engineer specializing in building scalable web
-                applications. With over 3 years of experience working with
-                clients across various industries, delivering high-performance
-                and user-centric software. Passionate about innovation and
-                problem-solving, combining technical excellence with a keen eye
-                for user experience.
+            <Grid
+              item
+              xs={6}
+              style={{
+                display: "flex",
+                justifyContent: "start",
+                flexDirection: "column",
+                alignItems: "flex-end",
+                alignSelf: "flex-start",
+                height: "auto",
+              }}
+            >
+              <Typography id="header-right-title">@ 2025</Typography>
+              <Typography id="header-right-subtitle">
+                All rights reserved.
               </Typography>
             </Grid>
           </Grid>
-          <Grid item xs={12}></Grid>
+
+          <Grid
+            item
+            style={{
+              flexGrow: 1,
+              display: "flex",
+              alignItems: "flex-start",
+              justifyContent: "flex-start",
+              flexDirection: "column",
+            }}
+          >
+            <Typography
+              className="typography-invert"
+              style={{
+                fontFamily: "NeueHaasDisplayLight",
+                fontSize: "55px",
+                color: "#fff",
+                textTransform: "uppercase",
+                lineHeight: 1,
+                padding: "20px 15px 0 15px",
+                textAlign: "left",
+              }}
+            >
+              Simona
+              <br />
+              Anchova
+            </Typography>
+
+            <Typography
+              className="typography-invert"
+              style={{
+                fontFamily: "Inter-Thin",
+                fontSize: "16px",
+                color: "#fff",
+                lineHeight: 1.1,
+                padding: 0,
+                textAlign: "justify",
+                padding: "0 15px 0 15px",
+              }}
+            >
+              Skilled Software Engineer specializing in building scalable web
+              applications. With over 3 years of experience working with clients
+              across various industries, delivering high-performance and
+              user-centric software. Passionate about innovation and
+              problem-solving, combining technical excellence with a keen eye
+              for user experience.
+            </Typography>
+          </Grid>
+
+          <Grid
+            item
+            style={{
+              padding: "16px",
+              justifyContent: "space-around",
+              display: "flex",
+            }}
+          >
+            {items.map((item) => (
+              <div
+                className="header-outside-link-div"
+                onClick={() => {
+                  if (item.name === "Contact") {
+                    window.location.href = item.link;
+                    return;
+                  } else {
+                    window.open(item.link, "_blank");
+                  }
+                }}
+              >
+                <Typography id="header-outside-link">
+                  [{item.name.slice(0, 1)}] {item.name}
+                </Typography>
+              </div>
+            ))}
+          </Grid>
         </Grid>
       </Hidden>
     </>
